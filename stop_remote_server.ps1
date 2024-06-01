c6ff786347b3e276c7ac1001df7cdecc3397c3cd
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-06-01 08:35:59 +0800

    Add files via upload

---
+++ stop_remote_server.ps1
@@ -0,0 +1,8 @@
+﻿$Shenzhen_Stage_servers = Get-Content -Path D:\Shenzhen_Stage_ServerList.txt
+foreach ($oneserver in $Shenzhen_Stage_servers)
+{
+    Write-Host "Currently, we are trying to shutdown the server" $oneserver
+    Stop-Computer -ComputerName $oneserver -Force
+
+}
+
