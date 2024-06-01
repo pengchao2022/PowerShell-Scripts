14222071bea8e77e201c7905d68c7e6dba95092a
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-06-01 08:50:29 +0800

    Add files via upload

---
+++ Powershell_To_Get_C_Driver_Space.ps1
@@ -0,0 +1,8 @@
+﻿$allservers =get-content -path "D:\Server_List\RG_List.txt"
+foreach ($oneserver in $allservers)
+{
+$disk = Get-WmiObject Win32_LogicalDisk -ComputerName $oneserver -Filter "DeviceID='C:'" | Select-Object Size,FreeSpace
+
+}
+$disk.Size
+$disk.FreeSpace
