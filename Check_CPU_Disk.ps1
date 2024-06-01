73a7037f97ffeb5d6d12a59de7cd0183ac8f8f51
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-06-01 08:31:21 +0800

    Add files via upload

---
+++ Check_CPU_Disk.ps1
@@ -0,0 +1,9 @@
+﻿$CPUavg = Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select-Object -ExpandProperty Average
+$mem = Get-WmiObject win32_operatingsystem | Foreach {"{0:N2}" -f ((($_.TotalVisibleMemorySize - $_.FreePhysicalMemory)*100)/ $_.TotalVisibleMemorySize)}
+$free = Get-WmiObject Win32_Volume -Filter "DriveLetter = 'C:'" | Foreach {"{0:N2}" -f (($_.FreeSpace / $_.Capacity)*100)}
+new-object psobject -prop @{
+            ServerName = $env:computername
+            AvgCpu = "$CPUavg%"
+            MemoryUsage = "$mem%"
+            OSDriveUsage = "$free%"
+        }
