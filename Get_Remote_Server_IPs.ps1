c019d7ecb6c79260a45bc02c12e962afb566031b
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2024-06-01 08:38:28 +0800

    Add files via upload

---
+++ Get_Remote_Server_IPs.ps1
@@ -0,0 +1,40 @@
+﻿$ServerName = Get-Content -path "D:\SV_server_list.txt"
+$index = @()
+$ping = New-Object System.Net.NetworkInformation.Ping
+
+Foreach($Server in $ServerName)
+{
+    $ipaddress = $null
+    $Object = $null
+    $Value = $null
+ 
+    
+    $ipaddress = ($ping.Send($Server).Address)
+ 
+    If($ipaddress)
+    {
+        $Value = $ipaddress.IPAddressToString
+    }
+    Else
+    {
+        $Value = "The IP Address is not found"
+    }
+ 
+
+    $Object = New-Object PSObject -Property ([ordered]@{ 
+ 
+            Server                  = $Server
+            IPAddress               = $Value
+ 
+        }) 
+          
+    $index += $Object
+ 
+    $Object
+}
+ 
+If($index)
+{
+
+    $index | Export-Csv -Path D:\Server_With_IP.CSV -NoTypeInformation
+}
